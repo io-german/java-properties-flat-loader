588eeb2ed764d02e422efc670bb9a2761b6404cc
Lukas Krecan <lukas.krecan@adleritech.net>
2019-03-27 17:12:42 +0100

    Removing unnecessary dependencies

--- src/propertiesReader.js
+++ src/propertiesReader.js
@@ -1,9 +1,7 @@
 'use strict';
 
-var sync = require('synchronize');
 var properties = require('properties');
 
 module.exports = function (source) {
-  sync(properties, 'parse');
   return properties.parse(source);
 };
